4ebab1fca9fb6b935e33743527b631cda91b584d
Mason Schleider <MasonSchleider@gmail.com>
2020-01-12 02:43:44 -0500

    Add break delay to reduce the chance of accidental breaks

--- src/main/java/com/masonschleider/breakbedrock/Main.java
+++ src/main/java/com/masonschleider/breakbedrock/Main.java
@@ -1,9 +1,7 @@
 package com.masonschleider.breakbedrock;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
@@ -15,25 +13,56 @@ import org.apache.logging.log4j.Logger;
 @Mod(Main.MODID)
 @Mod.EventBusSubscriber(modid = Main.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public final class Main {
-    public static final String MODID = "breakbedrock";
+    static final String MODID = "breakbedrock";
     
+    private static final long BREAK_PERIOD = 1250;
+    private static final long BREAK_TIMEOUT = 125;
     private static final Logger LOGGER = LogManager.getLogger(Main.MODID);
+    
+    private static int breakDelay = 0;
+    private static long clickDuration = 0;
+    private static long lastClickTime = 0;
     
     @SubscribeEvent
     public static void onPlayerLeftClickBlock(PlayerInteractEvent.LeftClickBlock event) {
+        World world = event.getPlayer().world;
+    
+        if (breakDelay > 0) {
+            if (world.isRemote) {
+                breakDelay--;
+                lastClickTime = System.currentTimeMillis();
+            }
+            event.setCanceled(true);
+            return;
+        }
+    
         BlockPos blockPos = event.getPos();
-        PlayerEntity playerEntity = event.getPlayer();
-        World world = playerEntity.world;
-        
-        if (world.isRemote || blockPos.getY() == 0)
-            return;
-        
         BlockState blockState = world.getBlockState(blockPos);
-        Block block = blockState.getBlock();
-        
-        if (block != Blocks.BEDROCK)
+    
+        if (blockState.getBlock() != Blocks.BEDROCK || blockPos.getY() == 0)
             return;
         
-        world.destroyBlock(blockPos, false);
+        if (world.isRemote) {
+            long currentTimeMillis = System.currentTimeMillis();
+            long deltaTime = currentTimeMillis - lastClickTime;
+    
+            LOGGER.info("breakTime: " + clickDuration + ", delta: " + deltaTime);
+    
+            if (deltaTime > BREAK_TIMEOUT) {
+                clickDuration = 0;
+                event.setCanceled(true);
+            } else if (clickDuration < BREAK_PERIOD) {
+                clickDuration += deltaTime;
+                event.setCanceled(true);
+            }
+    
+            lastClickTime = currentTimeMillis;
+        } else if (clickDuration >= BREAK_PERIOD) {
+            breakDelay = 5;
+            clickDuration = 0;
+            world.destroyBlock(blockPos, false);
+        } else {
+            event.setCanceled(true);
+        }
     }
 }
